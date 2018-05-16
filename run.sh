161326864c56bba7c05250283a473b0028b339a8
kenmaz <kentaro.matsumae@gmail.com>
2018-05-16 11:04:57 +0900

    fix

--- run.sh
+++ run.sh
@@ -4,9 +4,10 @@ rm -rf out
 mkdir out
 python src/train.py out/ out/ data/vctk/speaker1/vctk-speaker1-train.4.16000.8192.4096.h5 data/vctk/speaker1/vctk-speaker1-val.4.16000.8192.4096.h5 
 
-TAR_FILE=result.2018-05-16.tar
-IMG_FILE=img.2018-05-16.tar
-S3_DIR=s3://tryswift/audio-super-resolution/2018-05-16
+DATE=`date "+%Y%m%d-%H%M%S"`
+TAR_FILE=result.$DATE.tar
+IMG_FILE=img.$DATE.tar
+S3_DIR=s3://tryswift/audio-super-resolution/$DATE
 
 tar cvf $TAR_FILE out/events.* out/model.h5
 aws s3 cp $TAR_FILE $S3_DIR/$TAR_FILE.
