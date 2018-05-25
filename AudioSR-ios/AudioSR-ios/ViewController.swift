d6bcd358d20bb9fff6f69ca3498440eda89a50e7
kenmaz <kentaro.matsumae@gmail.com>
2018-05-25 11:10:18 +0900

    入力の配列長が400以上の場合、実機ではGPUで処理できない、というエラーが出るのでcpuを使う

--- AudioSR-ios/AudioSR-ios/ViewController.swift
+++ AudioSR-ios/AudioSR-ios/ViewController.swift
@@ -15,16 +15,21 @@ class ViewController: UIViewController {
 
         let lenght = 3
         //let wavData: [Float] = Array(repeating: 0, count: lenght)
-        let wavData: [Float] = [0,0,1,]
+        let wavData: [Float] = [0,1,0]
         print(wavData.count)
         let ptr = UnsafeMutablePointer(mutating: wavData)
         do {
-            let input = try MLMultiArray(dataPointer: ptr, shape: [1, 3, 1], dataType: MLMultiArrayDataType.float32, strides: [1,1,1], deallocator: nil)
+            let input = try MLMultiArray(dataPointer: ptr, shape: [1, NSNumber(integerLiteral: lenght), 1], dataType: MLMultiArrayDataType.float32, strides: [1,1,1], deallocator: nil)
             print(input)
+            
+            let wav = AudioSRInput(wav: input)
             
             do {
                 let model = AudioSR()
-                let output = try model.prediction(wav: input)
+                let opt = MLPredictionOptions()
+                opt.usesCPUOnly = true
+                let outFeatures = try model.model.prediction(from: wav, options: opt)
+                let output = AudioSROutput(output1: outFeatures.featureValue(for: "output1")!.multiArrayValue!)
                 print(output.output1)
             } catch {
                 print(error)
